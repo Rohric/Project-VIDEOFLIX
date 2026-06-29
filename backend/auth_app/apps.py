d647431f393cb287415320c4f1e0ca3008bb512d
Emil Marsal <emilmarsal@icloud.com>
2026-06-29 21:16:21 +0200

    delete all for new start

--- backend/auth_app/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class AuthAppConfig(AppConfig):
-    name = "auth_app"
-
-    def ready(self):
-        import auth_app.signals  # noqa: F401
--- backend/video_app/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class VideoAppConfig(AppConfig):
-    name = "video_app"
-
-    def ready(self):
-        from . import signals  # noqa: F401
